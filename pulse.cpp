bb414fda588bea88f7fdd4e6020106a5dd7aef9b
Ameya <ameya.marakarkandy@gmail.com>
2022-04-04 21:34:50 +0530

    Create pulse.cpp
    Turtle draws a pulse for even inputs

---
+++ pulse.cpp
@@ -0,0 +1,90 @@
+#include <simplecpp>
+main_program{
+	turtleSim();
+	int n1;
+	int n2;
+	int n3;
+	int n4;
+	int n5;
+	int n6;
+	int n7;
+	int n8;
+
+    cin>>n1>>n2>>n3>>n4>>n5>>n6>>n7>>n8;
+//space seperated inputs
+//Pulse created for even inputs and flat for odd inputs
+    if(n1%2 == 0){
+    	right(-90);
+    	repeat(3){
+    		forward(25); right(90);
+    	}
+    	right(180);
+    }
+    else forward(25);
+
+     if(n2%2 == 0){
+    	right(-90);
+    	repeat(3){
+    		forward(25); right(90);
+    	}
+    	right(180);
+    }
+    else forward(25);
+
+     if(n3%2 == 0){
+    	right(-90);
+    	repeat(3){
+    		forward(25); right(90);
+    	}
+    	right(180);
+    }
+    else forward(25);
+
+     if(n4%2 == 0){
+    	right(-90);
+    	repeat(3){
+    		forward(25); right(90);
+    	}
+    	right(180);
+    }
+    else forward(25);
+
+     if(n5%2 == 0){
+    	right(-90);
+    	repeat(3){
+    		forward(25); right(90);
+    	}
+    	right(180);
+    }
+    else forward(25);
+
+     if(n6%2 == 0){
+    	right(-90);
+    	repeat(3){
+    		forward(25); right(90);
+    	}
+    	right(180);
+    }
+    else forward(25);
+
+     if(n7%2 == 0){
+    	right(-90);
+    	repeat(3){
+    		forward(25); right(90);
+    	}
+    	right(180);
+    }
+    else forward(25);
+
+     if(n8%2 == 0){
+    	right(-90);
+    	repeat(3){
+    		forward(25); right(90);
+    	}
+    	right(180);
+    }
+    else forward(25);
+    wait(10);
+
+   
+}
